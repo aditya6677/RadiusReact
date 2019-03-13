50d32f5413c8422f3cdfc1845b4a85e4cc1b77a4
sandeep <sandeepkumar@holidayiq.com>
2019-03-13 14:58:02 +0530

    added default repo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     super(props);
     this.state = {
       isLoading: false,
-      repoName : '',
+      repoName : 'https://github.com/facebook/react',
       data : null
     };
   }
